b3df6d8afd27496ff77e38bb79ba0c748a11e90f
Jeffnguyen2601 <s3979209@rmit.edu.vn>
2024-09-06 19:30:16 +0700

    add Car class

---
+++ Car.java
@@ -0,0 +1,113 @@
+package DealingCar;
+
+import java.security.Provider;
+import java.util.List;
+
+public class Car {
+    private String carId;      // Unique ID for the car
+    private String brand;       // DealingCar.Car manufacturer
+    private String model;      // Model of the car
+    private int year;          // Year of manufacture
+    private long mileage;      // Mileage in kilometers
+    private String color;      // DealingCar.Car color
+    private boolean status;    // true for available, false for sold
+    private double price;      // Price of the car
+    private String notes;      // Additional information (e.g., service history)
+
+    private List<Provider.Service> serviceHistory;  // List to track service history
+
+    public Car(String carId, String brand, String model, int year, long mileage, String color, boolean status, double price, String notes, List<Provider.Service> serviceHistory) {
+        this.carId = carId;
+        this.brand = brand;
+        this.model = model;
+        this.year = year;
+        this.mileage = mileage;
+        this.color = color;
+        this.status = status;
+        this.price = price;
+        this.notes = notes;
+        this.serviceHistory = serviceHistory;
+    }
+
+    protected String getCarId() {
+        return carId;
+    }
+
+    protected  void setCarId(String carId) {
+        this.carId = carId;
+    }
+
+    protected String getbrand() {
+        return brand;
+    }
+
+    protected void setbrand(String brand) {
+        this.brand = brand;
+    }
+
+    protected String getModel() {
+        return model;
+    }
+
+    protected void setModel(String model) {
+        this.model = model;
+    }
+
+    protected int getYear() {
+        return year;
+    }
+
+    protected void setYear(int year) {
+        this.year = year;
+    }
+
+    protected long getMileage() {
+        return mileage;
+    }
+
+    protected void setMileage(long mileage) {
+        this.mileage = mileage;
+    }
+
+    protected String getColor() {
+        return color;
+    }
+
+    protected void setColor(String color) {
+        this.color = color;
+    }
+
+    protected boolean isStatus() {
+        return status;
+    }
+
+    protected void setStatus(boolean status) {
+        this.status = status;
+    }
+
+    protected double getPrice() {
+        return price;
+    }
+
+    protected void setPrice(double price) {
+        this.price = price;
+    }
+
+    protected String getNotes() {
+        return notes;
+    }
+
+    protected void setNotes(String notes) {
+        this.notes = notes;
+    }
+
+    protected List<Provider.Service> getServiceHistory() {
+        return serviceHistory;
+    }
+
+    protected void setServiceHistory(List<Provider.Service> serviceHistory) {
+        this.serviceHistory = serviceHistory;
+    }
+
+
+}
