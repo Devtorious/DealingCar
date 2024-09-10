cf145fd753c300332d678aa3a292dfb798d85edd
s397976 <s3979760@rmit.edu.vn>
2024-09-10 13:49:46 +0700

    Update client.java

--- DealingCar/client.java
+++ DealingCar/client.java
@@ -1,4 +1,40 @@
 package DealingCar;
 
 public class client {
+    public int spending;
+    public String membership;
+    public int discountrate;
+
+    public int getSpending() {
+        return spending;
+    }
+
+    public void setSpending(int spending) {
+        this.spending = spending;
+    }
+
+    public String getMembership() {
+        return membership;
+    }
+
+    public void setMembership(String membership) {
+        this.membership = membership;
+    }
+
+    public int getDiscountrate() {
+        return discountrate;
+    }
+
+    public void setDiscountrate(int discountrate) {
+        this.discountrate = discountrate;
+    }
+
+    @Override
+    public String toString() {
+        return "client{" +
+                "spending=" + spending +
+                ", membership='" + membership + '\'' +
+                ", discountrate=" + discountrate +
+                '}';
+    }
 }
