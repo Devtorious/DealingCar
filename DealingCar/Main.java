83890ca14700b4fdacfa14afa9be4270bacaebb3
Jeffnguyen2601 <s3979209@rmit.edu.vn>
2024-09-18 02:05:29 +0700

    small edit

--- DealingCar/Main.java
+++ DealingCar/Main.java
@@ -12,7 +12,7 @@ public class Main {
      }
 
     public static void main(String[] args) {
-        System.out.println(" _______________________________________________");
+        System.out.println(" _____________________________________________");
         System.out.println("| COSC2081 GROUP ASSIGNMENT                   |");
         System.out.println("| AUTO168 CAR DEALERSHIP MANAGEMENT SYSTEM    |");
         System.out.println("| Instructor: Mr. Minh Vu & Mr. Dung Nguyen   |");
